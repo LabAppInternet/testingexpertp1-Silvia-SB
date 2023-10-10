b6ef5c2452d9427aa357da1bad97ab47daf660b3
Silvia-SB <103179506+Silvia-SB@users.noreply.github.com>
2023-10-10 12:03:11 +0200

    començament

--- src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
+++ src/test/java/cat/tecnocampus/fgcstations/FgCstationsApplicationTests.java
@@ -1,13 +1,47 @@
 package cat.tecnocampus.fgcstations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FgCstationsApplicationTests {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void contextLoads() {
     }
+
+@Test
+@Order(1)
+void testGetUser() throws Exception{
+    mockMvc.perform(get("/users/joanra"))
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$", hasSize(5)));
+
+}
 
 }
